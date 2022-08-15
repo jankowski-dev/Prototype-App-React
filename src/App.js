9a09634666a7eb189192b15781867fcfa31905d7
jankowskij <volggalla@gmail.com>
2022-08-15 14:01:28 +0300

    twenty commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Fuck Yourself!
+          Edit <code>src/App.js</code> and save to reload. Fuck Yourself motherfucker!
         </p>
         <a
           className="App-link"
